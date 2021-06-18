c42d093118d71a2fe9a756c3654a857a620a0309
TerenceJDew <terence.dew@gmail.com>
2021-06-18 00:24:41 -0400

    Initial Commit

---
+++ main.py
@@ -0,0 +1,100 @@
+from flask import Flask, jsonify, request, render_template
+from flask_mail import Mail, Message
+from flask_pymongo import PyMongo
+from flask_cors import CORS, cross_origin
+from bson.json_util import dumps, loads
+
+import re
+
+from pymongo.collection import ReturnDocument
+
+app = Flask(__name__)
+CORS(app)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/bcss_survey"
+mongo = PyMongo(app)
+
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USERNAME"] = "scobigcorp878@gmail.com"
+app.config["MAIL_PASSWORD"] = "Orchid20?!?!"
+app.config["MAIL_USE_TLS"] = False
+app.config["MAIL_USE_SSL"] = True
+mail = Mail(app)
+
+
+def send_mail(to, subject, template, **kwargs):
+    msg = Message(
+        subject=subject, sender="bcscoopsshop@gmail.com", recipients=to.split()
+    )
+    msg.body = "Please Enjoy Your Free Coupon from BC Scoop Shop"
+    msg.html = render_template(template, **kwargs)
+    mail.send(msg)
+
+
+def parse_json(data):
+    return loads(dumps(data))
+
+
+@app.get("/")
+def get_home():
+    return "Why are you here? This isn't the data you were looking for"
+
+
+@app.post("/submissions")
+def post_answers():
+    request_data = request.json
+    sanitized_data = parse_json(request_data)
+    print(sanitized_data)
+    mongo.db.submissions.find_one_and_update(
+        {"email": request_data["email"]},
+        {"$set": sanitized_data},
+        upsert=True,
+        return_document=ReturnDocument.AFTER,
+    )
+    send_mail(request_data["email"], "Your Free Coupon", "coupon.html")
+    return request_data
+
+
+@app.get("/questions")
+def get_questions():
+    email = request.args["email"]
+    content = {
+        "treattype": {
+            "prompt": "Select your frozen treat of choice:",
+            "choices": ["Soft-Serve", "Hard(Scooped)", "Frozen Yogurt"],
+        },
+        "flavor": {
+            "prompt": "Select your favorite flavor",
+            "choices": ["Chocolate", "Vanilla", "Twist"],
+        },
+        "scoopflavor": {
+            "prompt": "Select your favorite Scoop flavor",
+            "choices": [
+                "Chocolate",
+                "Vanilla",
+                "Strawberry",
+                "Mint",
+                "Green" "Tea",
+            ],
+        },
+        "cupcone": {
+            "prompt": "Select what you like to hold your ice cream",
+            "choices": ["Cup", "Cone"],
+        },
+        "topping": {
+            "prompt": "Select your Topping of Choice",
+            "choices": ["Sprinkles", "Hot Fudge", "Dip"],
+        },
+        "dip": {
+            "prompt": "Select your Dip Flavor of Choice",
+            "choices": ["Chocolate", "Cherry"],
+        },
+    }
+
+    existing_record = mongo.db.submissions.find_one({"email": email})
+    if existing_record is not None:
+        del existing_record["_id"]
+        content.update({"answers": existing_record})
+    print(content)
+
+    return jsonify(content)
